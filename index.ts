00f47056750d4133c226b8601b96f6ab4817722d
Bruno Braga <brunobragaw8t@gmail.com>
2023-02-12 10:05:54 +0000

    part 2

--- index.ts
+++ index.ts
@@ -4,6 +4,7 @@ fs.readFile("./input.txt", "utf8", (_, data) => {
   const steps = data.split("");
 
   let floor = 0;
+  let firstTimeInBasement = 0;
 
   for (let i = 0; i < steps.length; i++) {
     if ("(" === steps[i]) {
@@ -11,7 +12,12 @@ fs.readFile("./input.txt", "utf8", (_, data) => {
     } else {
       floor--;
     }
+
+    if (-1 === floor && 0 === firstTimeInBasement) {
+      firstTimeInBasement = i + 1;
+    }
   }
 
   console.log(floor);
+  console.log(firstTimeInBasement);
 });
